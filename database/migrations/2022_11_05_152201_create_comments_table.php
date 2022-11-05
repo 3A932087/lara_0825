54bf891b4742799753e679a8fab9441c3133a9cd
Chen Yi-Hsuan <s3a932087@student.ncut.edu.tw>
2022-11-05 23:37:41 +0800

    使用migration建立comments資料表

---
+++ database/migrations/2022_11_05_152201_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigInteger('id');
+            $table->text('content');
+            $table->bigInteger('post_id');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();//->default(\DB::raw('CURRENT_TIMESTAMP'))
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
